9d0af220a2a53b8001fad7aa8b38aed9c8f83c57
yotam <yotamolenik@gmail.com>
2020-06-04 21:46:31 +0300

    part 3 works for real(with undefined error)

--- 204051115_311130561/part3/part3.ts
+++ 204051115_311130561/part3/part3.ts
@@ -1,3 +1,60 @@
+
+// given 2 generators, interleave their values. if 1 of them is out of values, take from the other one
+// finish when they are both out of values
+export function* braid(generator1: Generator, generator2: Generator){
+    const gen1Op = generator1;
+    const gen2Op = generator2;
+    for(let n=0;; n++){
+        let x = gen1Op.next()
+        let y = gen2Op.next()
+        if(! (x.value === undefined) )
+            yield x.value
+        if(! (y.value === undefined) )
+            yield y.value
+        if ((x.value === undefined)&&(y.value === undefined))
+            return x.value
+    }
+}
+
+
+export function* biased(generator1: Generator, generator2: Generator){
+    const gen1Op = generator1;
+    const gen2Op = generator2;
+    for(let n=0;; n++){
+        let x = gen1Op.next()
+        let y = gen2Op.next()
+        if(! (x.value === undefined) )
+            yield x.value;
+        x = gen1Op.next()
+        if(! (x.value === undefined) )
+            yield x.value;
+        if(! (y.value === undefined) )
+            yield y.value;
+        if ((x.value === undefined) && (y.value === undefined))
+            return x.value
+    }
+}
+
+
+/*
+function* take(n:number, generator:Generator) {
+    for (let x of generator) {
+        if (n <= 0) return;
+        n--;
+        yield x;
+    }
+}
+
+
+
+function* foo() {
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
+    yield 5;
+    return 6;
+}
 function* idMaker() {
     let index = 0;
     while (index < 3)
@@ -10,35 +67,6 @@ function* idMaker() {
   console.log(gen.next().value); // 1
   console.log(gen.next().value); // 2
   console.log(gen.next().value); // undefined
-
-
-
-export function* braid(generator1: Generator, generator2: Generator){
-    for(let n=0;; n++){
-        yield generator1.next();
-        yield generator2.next();
-    }
-}
-
-export function* biased(generator1: Generator, generator2: Generator){
-    for(let n=0;; n++){
-        yield generator1;
-        yield generator1;
-        yield generator2;
-    }
-}
-
-/*
-function* foo() {
-    yield 1;
-    yield 2;
-    yield 3;
-    yield 4;
-    yield 5;
-    return 6;
-}
-
-
 
 // const readFilePromise = (filename: string): Promise<string> => {
 //     return new Promise<string>( (resolve, reject) => {
@@ -82,13 +110,7 @@ for ( let n=0; n<5;n++ ) {
 
 
 /*
-function* take(n, generator) {
-    for (let x of generator) {
-        if (n <= 0) return;
-        n--;
-        yield x;
-    }
-}
+
 
 
 
